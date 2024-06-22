bfbdd48bcca11e30c4624d1ffea286c24c8f0f35
diego411 <diego.enrico@directbox.com>
2024-06-22 16:14:39 +0200

    use dropbox refresh token

--- dropbox_controller.py
+++ dropbox_controller.py
@@ -3,12 +3,20 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DROPBOX_ACCESS_TOKEN = os.environ.get("DROPBOX_ACCESS_TOKEN")
-dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+DROPBOX_REFRESH_TOKEN = os.environ.get("DROPBOX_REFRESH_TOKEN")
+DROPBOX_APP_KEY = os.environ.get("DROPBOX_APP_KEY")
+DROPBOX_APP_SECRET = os.environ.get("DROPBOX_APP_SECRET")
+dbx = dropbox.Dropbox(
+    app_key=DROPBOX_APP_KEY,
+    app_secret=DROPBOX_APP_SECRET,
+    oauth2_refresh_token=DROPBOX_REFRESH_TOKEN
+)
 
 
 def upload_file_to_dropbox(file_path, dropbox_path):
     global dbx
+
+    dbx.check_and_refresh_access_token()
 
     try:
         # Open the file and upload it
