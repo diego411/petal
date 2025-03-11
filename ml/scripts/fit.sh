798177ef08254315fe62c1fb0e45889d5d9d1529
diego411 <diego.enrico@directbox.com>
2025-03-11 21:09:33 +0000

    65: setup more logging stuff

--- ml/scripts/fit.sh
+++ ml/scripts/fit.sh
@@ -1,2 +1,7 @@
-python -m ml.model_cli fit --config ml/scripts/config.yaml
-    
+#!/bin/bash
+CONFIG_FILE="ml/scripts/config.yaml"
+EXPERIMENT_HASH=$(python -m ml.scripts.generate_experiment_name "$CONFIG_FILE")
+EXPERIMENT_PATH="ml/experiments/$EXPERIMENT_HASH"
+EXPERIMENT_VERSION=$(python -m ml.scripts.generate_experiment_version $EXPERIMENT_PATH)
+
+python -m ml.model_cli fit --config $CONFIG_FILE --trainer.logger.save_dir=$EXPERIMENT_PATH --trainer.callbacks.dirpath=ml/experiments/$EXPERIMENT_HASH/$EXPERIMENT_VERSION/checkpoints
---
+++ ml/scripts/generate_experiment_version.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import os
+import sys
+
+def get_experiment_version(path_str: str):
+    if not os.path.exists(path_str):
+        return 'version_0'
+    
+    path: Path = Path(path_str)
+    directories = [dir for dir in path.iterdir() if dir.is_dir() and dir.stem.startswith('v')]
+    return f'version_{len(directories)}'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: generate_experiment_version.py <path>")
+        sys.exit(1)
+
+    version = get_experiment_version(sys.argv[1])
+    print(version)
