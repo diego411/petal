f2b6779fec3300303e5441f0fa11ceeba953d29c
diego411 <diego.enrico@directbox.com>
2025-03-12 13:51:50 +0000

    65: improve logging dir structure and log confusion matrix, precision recall curve and roc curve

--- ml/models/ResCNNetLightning.py
+++ ml/models/ResCNNetLightning.py
@@ -1,10 +1,16 @@
 import lightning.pytorch as L
+from lightning.pytorch.loggers import CSVLogger, Logger
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
-from torchmetrics import Accuracy, Precision, Recall, F1Score, AUROC
+from torchmetrics import Accuracy, Precision, Recall, F1Score, AUROC, ConfusionMatrix, PrecisionRecallCurve, ROC
 from torch import Tensor
 from typing import Tuple
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from typing import List, Optional
+from pathlib import Path
 
 class ResCNNetLightning(L.LightningModule):
     def __init__(self, n_output=1, freeze=True, lr=1e-4): # TODO: passing params through cli doesnt work
@@ -30,6 +36,9 @@ class ResCNNetLightning(L.LightningModule):
         self.recall = Recall(task="binary" if n_output == 1 else "multiclass", num_classes=n_output)
         self.f1 = F1Score(task="binary" if n_output == 1 else "multiclass", num_classes=n_output, average="weighted")
         self.auroc = AUROC(task="binary" if n_output == 1 else "multiclass", num_classes=n_output)
+        self.confusion_matrix = ConfusionMatrix(task="binary" if n_output == 1 else "multiclass", num_classes=n_output) 
+        self.precision_recall_curve = PrecisionRecallCurve(task="binary" if n_output == 1 else "multiclass", num_classes=n_output)
+        self.roc_curve = ROC(task="binary" if n_output == 1 else "multiclass", num_classes=n_output)
         
     def forward(self, x) -> Tensor:
         x = self.resnet(x).squeeze()
@@ -54,6 +63,9 @@ class ResCNNetLightning(L.LightningModule):
         self.recall.update(predictions, targets)
         self.f1.update(predictions, targets)
         self.auroc.update(predictions, targets)
+        self.confusion_matrix.update(predictions, targets)
+        self.precision_recall_curve.update(predictions, targets)
+        self.roc_curve.update(predictions, targets)
     
     def _log_metrics(self, stage: str):
         self.log(f"{stage}_accuracy", self.accuracy.compute())
@@ -62,12 +74,88 @@ class ResCNNetLightning(L.LightningModule):
         self.log(f"{stage}_f1", self.f1.compute())
         self.log(f"{stage}_auroc", self.auroc.compute())
 
+        csv_logger = self.get_csv_logger()
+        if csv_logger is None:
+            return
+        if csv_logger.log_dir is None:
+            return
+        log_dir = Path(csv_logger.log_dir) / 'files'
+        os.makedirs(log_dir, mode=0o777, exist_ok=True)
+
+        current_epoch = self.trainer.current_epoch
+        self.log_confusion_matrix(stage, log_dir, current_epoch)
+        self.log_precision_recall_curve(stage, log_dir, current_epoch)
+        self.log_roc_curve(stage, log_dir, current_epoch)
+
     def _reset_metrics(self):
         self.accuracy.reset()
         self.precision.reset()
         self.recall.reset()
         self.f1.reset()
         self.auroc.reset()
+        self.confusion_matrix.reset()
+        self.precision_recall_curve.reset()
+        self.roc_curve.reset()
+    
+    def log_confusion_matrix(self, stage: str, log_dir: Path, current_epoch: int):
+        conf_matrix = self.confusion_matrix.compute()
+
+        fig, ax = plt.subplots()
+        sns.heatmap(conf_matrix.cpu().numpy(), annot=True, fmt="d", cmap="Blues", ax=ax)
+        ax.set_xlabel('Predicted labels')
+        ax.set_ylabel('True labels')
+        ax.set_title(f'Confusion Matrix {stage} Epoch {current_epoch}')
+
+        self.log(
+            f'{stage}_true_negatives',
+            conf_matrix[0, 0].item(),
+            sync_dist=True
+        )
+        self.log(
+            f'{stage}_false_positives',
+            conf_matrix[0, 1].item(),
+            sync_dist=True
+        )
+        self.log(
+            f'{stage}_false_negatives',
+            conf_matrix[1, 0].item(),
+            sync_dist=True
+        )
+        self.log(
+            f'{stage}_true_positives',
+            conf_matrix[1, 1].item(),
+            sync_dist=True
+        )
+        
+        confusion_matrices_dir = log_dir / 'confusion_matrices'
+        os.makedirs(confusion_matrices_dir, mode=0o777, exist_ok=True)
+
+        fig.savefig(confusion_matrices_dir / f'{stage}_confustion_matrix_epoch_{current_epoch}.png')
+        plt.close(fig)
+    
+    def log_precision_recall_curve(self, stage: str, log_dir: Path, current_epoch: int):
+        precision_recall_curves_dir = log_dir / 'precision_recall_curves'
+        os.makedirs(precision_recall_curves_dir, mode=0o777, exist_ok=True)
+
+        fig, ax = self.precision_recall_curve.plot(score=True)
+        fig.savefig(precision_recall_curves_dir / f"{stage}_precision_recall_curve_epoch_{current_epoch}.png", dpi=300)
+        plt.close(fig)
+
+    def log_roc_curve(self, stage: str, log_dir: Path, current_epoch: int):
+        roc_curves_dir = log_dir / 'roc_curves'
+        os.makedirs(roc_curves_dir, mode=0o777, exist_ok=True)
+
+        fig, ax = self.roc_curve.plot(score=True)
+        fig.savefig(roc_curves_dir / f"{stage}_roc_curve_epoch_{current_epoch}.png", dpi=300)
+        plt.close(fig)
+
+    def get_csv_logger(self) -> Optional[Logger]:
+        csv_loggers = list(filter(lambda logger: isinstance(logger, CSVLogger), self.trainer.loggers))
+
+        if len(csv_loggers) == 0:
+            return None
+        
+        return csv_loggers[0]
 
     def training_step(self, batch, batch_idx):
         _, _, loss = self.single_step(batch)
--- ml/scripts/fit.sh
+++ ml/scripts/fit.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 CONFIG_FILE="ml/scripts/config.yaml"
 EXPERIMENT_HASH=$(python -m ml.scripts.generate_experiment_name "$CONFIG_FILE")
-EXPERIMENT_PATH="ml/experiments/$EXPERIMENT_HASH"
+EXPERIMENTS_PATH="ml/experiments"
+EXPERIMENT_PATH="$EXPERIMENTS_PATH/$EXPERIMENT_HASH"
 EXPERIMENT_VERSION=$(python -m ml.scripts.generate_experiment_version $EXPERIMENT_PATH)
 
-python -m ml.model_cli fit --config $CONFIG_FILE --trainer.logger.save_dir=$EXPERIMENT_PATH --trainer.callbacks.dirpath=ml/experiments/$EXPERIMENT_HASH/$EXPERIMENT_VERSION/checkpoints
+python -m ml.model_cli fit \
+    --config $CONFIG_FILE \
+    --trainer.logger.save_dir=$EXPERIMENTS_PATH \
+    --trainer.logger.name=$EXPERIMENT_HASH \
+    --trainer.logger.version=$EXPERIMENT_VERSION \
+    --trainer.callbacks.dirpath=$EXPERIMENT_PATH/$EXPERIMENT_VERSION/checkpoints
