d38926570ac2c5229a7a6b4c69fb85558e114bb2
diego411 <diego.enrico@directbox.com>
2025-03-13 12:11:11 +0000

    67: configure where config.yaml is saved

--- ml/clis/vision_cnn_cli.py
+++ ml/clis/vision_cnn_cli.py
@@ -1,17 +1,34 @@
-from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.cli import LightningArgumentParser, LightningCLI, SaveConfigCallback
+from lightning.pytorch import LightningModule, Trainer
 from ml.models.VisionCNNet import VisionCNN 
 from ml.data.PetalDataModule import PetalDataModule
 import wandb
+import yaml
+from pathlib import Path
+from ml.utils.generate_experiment_name import hash_yaml
+import os
+
+class CustomSaveConfigCallback(SaveConfigCallback):
+    def save_config(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:    
+        config = self.parser.dump(self.config, skip_none=False)  # Required for proper reproducibility
+        yaml_data = yaml.safe_load(config)
+
+        path = Path('ml/experiments') / hash_yaml('ml/scripts/vision_cnn/config.yaml')
+        os.makedirs(path, mode=0o777, exist_ok=True)
+
+        with open(path / 'config.yaml', "w") as file:
+            yaml.dump(yaml_data, file, default_flow_style=False)
+
 
 # TODO: maybe one cli for everything will work?
 def cli_main() -> None:
-    wandb.login()
- 
     cli = LightningCLI(
         model_class=VisionCNN,
         datamodule_class=PetalDataModule,
-        save_config_kwargs={"overwrite": True},
+        save_config_kwargs={"save_to_log_dir": False},
+        save_config_callback=CustomSaveConfigCallback
     )
 
 if __name__ == '__main__':
+    wandb.login()
     cli_main()
