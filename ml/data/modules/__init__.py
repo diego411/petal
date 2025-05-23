62f147b6612b780708ed06c7bfed76d4e4b06a5d
diego411 <diego.enrico@directbox.com>
2025-05-23 08:58:21 +0000

    add CNN model

--- ml/data/modules/__init__.py
+++ ml/data/modules/__init__.py
@@ -1,5 +1,6 @@
 from .PetalDataModule import PetalDataModule
 from .VisionDataModule import VisionDataModule
 from .DeltaVisionDataModule import DeltaVisionDataModule
+from .CNNetDataModule import CNNetDataModule
 
-__all__ = ["PetalDataModule", "VisionDataModule", "DeltaVisionDataModule"]
+__all__ = ["PetalDataModule", "VisionDataModule", "DeltaVisionDataModule", "CNNetDataModule"]
--- ml/models/__init__.py
+++ ml/models/__init__.py
@@ -1,5 +1,6 @@
 from .PetalModule import PetalModule
 from .VisionCNN import VisionCNN
 from .DeltaVisionCNN import DeltaVisionCNN
+from .CNNet import CNNet
 
-__all__ = ['PetalModule', 'VisionCNN', 'DeltaVisionCNN']
+__all__ = ['PetalModule', 'VisionCNN', 'DeltaVisionCNN', 'CNNet']
