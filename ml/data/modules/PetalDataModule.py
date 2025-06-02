24c3dc8424545460dfca3bc6535b34189f715958
diego411 <diego.enrico@directbox.com>
2025-06-02 12:35:13 +0000

    ds01 code

--- ml/data/modules/PetalDataModule.py
+++ ml/data/modules/PetalDataModule.py
@@ -16,6 +16,7 @@ from typing import Optional, Dict
 from src.utils.file import find_file_path
 from collections import Counter
 import torch
+from ml.data.dataset_util import remove_class_samples
 
 class PetalDataModule(LightningDataModule):
 
@@ -28,8 +29,9 @@ class PetalDataModule(LightningDataModule):
         train_ratio:float=0.7,
         validation_ratio:float=0.1,
         augment_technique:Optional[str]=None,
-        augment_ratio:float=0,
+        augment_ratios:Optional[Dict[str, float]]=None,
         desired_distribution:Optional[Dict[str, float]]=None,
+        undersample_ratios:Optional[Dict[str, float]]=None,
         seed:int=42,
         batch_size:int=16,
         number_of_workers:int=1,
@@ -44,11 +46,12 @@ class PetalDataModule(LightningDataModule):
         self.train_ratio = train_ratio
         self.validation_ratio = validation_ratio 
         self.desired_distribution = desired_distribution
+        self.undersample_ratios = undersample_ratios
         self.seed = seed 
         self.batch_size = batch_size
         self.number_of_workers = number_of_workers
         self.verbose = verbose
-        self.augment_ratio = augment_ratio
+        self.augment_ratios = augment_ratios
 
         if dataset_type != 'pre-labeled' and dataset_type != 'post-labeled':
             raise MisconfigurationException("Invalid dataset type! Only pre-labeled and post-labeled are supported!")
@@ -115,6 +118,12 @@ class PetalDataModule(LightningDataModule):
         if self.augment_technique is None:
             return
 
+        assert self.augment_ratios is not None, "Provided augment technique but not augment ratios!"
+        try:
+            augment_ratios = {self.class_to_idx[key]: augment_ratio for key, augment_ratio in self.augment_ratios.items()}
+        except KeyError:
+            raise MisconfigurationException("Invalid class provided in augment_ratios parameter!")
+        
         print("[Datamodule] Starting data augmentation")
 
         augmented_samples: List[dict] = []
@@ -122,67 +131,69 @@ class PetalDataModule(LightningDataModule):
 
         random.shuffle(train_sample_paths)
 
-        for train_spectrogram_path, label_index in train_sample_paths[0:int(len(train_sample_paths) * self.augment_ratio)]:
-            train_spectrogram_path = Path(train_spectrogram_path)
-            if self.augment_technique['type'] == 'audio':
-                if self.binary:
-                    train_audio_path = find_file_path(
-                        file_name=train_spectrogram_path.with_suffix('.wav').name,
-                        directory=get_audio_path(self.dataset_type)
-                    )
-                    if train_audio_path is None:
-                        raise RuntimeError(f"Augmentation failed. Path to spectrogram was not found for following path: {train_spectrogram_path}")
-                else:
-                    train_audio_path = Path(str(train_spectrogram_path).replace('spectrograms', 'audio')).with_suffix('.wav') 
+        for class_idx, augment_ratio in augment_ratios.items():
+            samples_for_class = list(filter(lambda x: x[1] == class_idx, train_sample_paths))
+            for train_spectrogram_path, _ in samples_for_class[0:int(len(samples_for_class) * augment_ratio)]:
+                train_spectrogram_path = Path(train_spectrogram_path)
+                if self.augment_technique['type'] == 'audio':
+                    if self.binary:
+                        train_audio_path = find_file_path(
+                            file_name=train_spectrogram_path.with_suffix('.wav').name,
+                            directory=get_audio_path(self.dataset_type)
+                        )
+                        if train_audio_path is None:
+                            raise RuntimeError(f"Augmentation failed. Path to spectrogram was not found for following path: {train_spectrogram_path}")
+                    else:
+                        train_audio_path = Path(str(train_spectrogram_path).replace('spectrograms', 'audio')).with_suffix('.wav') 
 
-                augmented_audio_path = get_augment_audio_path(
-                    audio_path=train_audio_path,
-                    technique=self.augment_technique['label'],
-                    dataset_type=self.dataset_type
-                )
-
-                if not augmented_audio_path.exists():
-                    self.augment_technique['apply'](
+                    augmented_audio_path = get_augment_audio_path(
                         audio_path=train_audio_path,
-                        target_path=augmented_audio_path
+                        technique=self.augment_technique['label'],
+                        dataset_type=self.dataset_type
                     )
 
-                augmented_image_path = get_augment_image_path(
-                    image_path=train_audio_path,
-                    technique=self.augment_technique['label'],
-                    dataset_type=self.dataset_type
-                )
-                spectrogram_paths = generate_spectrogram_for(
-                    audio_path=augmented_audio_path,
-                    spectrogram_type=self.spectrogram_type,
-                    spectrogram_backend=self.spectrogram_backend,
-                    with_deltas=False,
-                    target_path=augmented_image_path.parent
-                )
-                sample = {
-                    'label_index': label_index,
-                    'paths': spectrogram_paths
-                }
-                augmented_samples.append(sample)
-            elif self.augment_technique['type'] == 'image':
-                augmented_image_path = get_augment_image_path(
-                    image_path=train_spectrogram_path,
-                    technique=self.augment_technique['label'],
-                    dataset_type=self.dataset_type
-                )
-                if not augmented_image_path.exists():
-                    self.augment_technique['apply'](
-                        image_path=train_spectrogram_path,
-                        target_path=augmented_image_path
+                    if not augmented_audio_path.exists():
+                        self.augment_technique['apply'](
+                            audio_path=train_audio_path,
+                            target_path=augmented_audio_path
+                        )
+
+                    augmented_image_path = get_augment_image_path(
+                        image_path=train_audio_path,
+                        technique=self.augment_technique['label'],
+                        dataset_type=self.dataset_type
                     )
-                sample = {
-                    'label_index': label_index,
-                    'paths': {
-                        'spectrogram_path': augmented_image_path
+                    spectrogram_paths = generate_spectrogram_for(
+                        audio_path=augmented_audio_path,
+                        spectrogram_type=self.spectrogram_type,
+                        spectrogram_backend=self.spectrogram_backend,
+                        with_deltas=False,
+                        target_path=augmented_image_path.parent
+                    )
+                    sample = {
+                        'label_index': class_idx,
+                        'paths': spectrogram_paths
                     }
-                }
-                augmented_samples.append(sample)
-        
+                    augmented_samples.append(sample)
+                elif self.augment_technique['type'] == 'image':
+                    augmented_image_path = get_augment_image_path(
+                        image_path=train_spectrogram_path,
+                        technique=self.augment_technique['label'],
+                        dataset_type=self.dataset_type
+                    )
+                    if not augmented_image_path.exists():
+                        self.augment_technique['apply'](
+                            image_path=train_spectrogram_path,
+                            target_path=augmented_image_path
+                        )
+                    sample = {
+                        'label_index': class_idx,
+                        'paths': {
+                            'spectrogram_path': augmented_image_path
+                        }
+                    }
+                    augmented_samples.append(sample)
+            
         return augmented_samples
     
     def _create_stratified_data_split(self, dataset: ImageFolder | SpectrogramDataset) -> Tuple[Subset, Subset, Subset]:
@@ -221,6 +232,19 @@ class PetalDataModule(LightningDataModule):
             stratify=temp_targets,
             random_state=self.seed
         )
+
+        if self.undersample_ratios is not None:
+            for class_name, ratio in self.undersample_ratios.items():
+                class_idx = self.class_to_idx[class_name]
+
+                target_mask = np.array(dataset.targets)[train_indices] == class_idx
+                target_indices = train_indices[target_mask]
+
+                n_keep = int(len(target_indices) * ratio)
+                np.random.seed(self.seed)
+                indices_to_keep = np.random.choice(target_indices, n_keep, replace=False)
+
+                train_indices = np.concatenate([train_indices[~target_mask], indices_to_keep])
         
         # Create subset datasets
         train_subset = Subset(dataset, train_indices)
